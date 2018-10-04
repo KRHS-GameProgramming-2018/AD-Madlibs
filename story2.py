9fdab13b6d968e3e99ca65b8d60611c2360d6bd7
Derek <derek.schoonmaker@kearsarge.org>
2018-10-04 08:06:22 -0400

    continued work on story

--- story2.py
+++ story2.py
@@ -7,15 +7,10 @@ def playMadlibs():
 
 output += " Story by Derek "
 output += "As I got up this morning, I decided to make pancakes for breakfast. "
-output += " First, I added " + number1 + " cups of flour"
-output +=
-output +=
-output +=
-output +=
-output +=
-output +=
-output +=
-output +=
+output += " First, I added " + number1 + " cups of flour."
+output += "Next came the " + color2 + " baking powder."
+output += " After mixing in the salt and sugar, I dumped in the 2 " + unitsofmeasure + #gallons, liter,cups..etc  " of milk"
+output += ""
 
 
 
