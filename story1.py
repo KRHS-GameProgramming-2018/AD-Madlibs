cef5a6ff4fc53427f57e0eb719b102266ecf897a
Aaron <acthayer@gmail.com>
2018-10-19 14:15:30 -0400

    Added car company BMW to car list. Added bodyCover getter to story1.

--- story1.py
+++ story1.py
@@ -5,6 +5,7 @@ def playMadlibs(sc):
     numAnimals = getNumber("Enter a number: ", 2, 10, sc)
     animals1 = getWord("Enter a pluaral animal name: ", sc)
     color1 = getWord("Enter a color: ", sc)
+    bodyCover = getWord("Enter a body cover, fur, skin, scales etc: ", sc)
     phone = getPhone("Enter phone: ", sc)
     mountainInNH = getMountain ("Enter the name of a mountain in NH: ", sc)
     day =  getDay ("Name a day of the week: ", sc)
@@ -13,7 +14,7 @@ def playMadlibs(sc):
     output += "One day I was walking with my friend, " + friend1
     output += ". Suddenly " + friend1
     output += " said that they saw " + numAnimals + " " + animals1 
-    output += " I turned and saw the animals my friend was talking about. To my suprise, they had " + color1 + " skin."
+    output += " I turned and saw the animals my friend was talking about. To my suprise, they had " + color1 + " " + bodyCover + "."
     output += " In my suprise, I pulled out my " + phone + " and took a picture. "
     output += " The next day, I printed out the photos I had taken yesterday and to my suprise, they were empty."
     output += " The next day, I went to see my friend to ask them about the wierd animals we had seen yesterday."
